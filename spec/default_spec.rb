795049c83e8787b5677edc96d2fd8efb262ae173
Chris Aumann <me@chr4.org>
2013-09-12 16:18:38 +0200

    remove default_spec

--- spec/default_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'chefspec'
-
-describe 'iptables-ng::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'iptables-ng::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
-  end
-end
