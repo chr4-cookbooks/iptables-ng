4d4e9256945e4f5f3618743c4f0d49a0e9cee3e3
Chris Aumann <me@chr4.org>
2013-09-13 11:16:32 +0200

    split up manage library

--- libraries/restart_service.rb
+++ libraries/restart_service.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: iptables-ng
-# Recipe:: manage
+# Recipe:: restart_service
 #
 # Copyright 2013, Chris Aumann
 #
@@ -23,58 +23,6 @@
 # subscribes / notifies. Therefore, using this workaround.
 module Iptables
   module Manage
-    def create_iptables_rules(ip_version)
-      rules = {}
-
-      # Retrieve all iptables rules for this ip_version,
-      # as well as default policies
-      Dir["/etc/iptables.d/*/*/*.rule_v#{ip_version}",
-          '/etc/iptables.d/*/*/default'].each do |path|
-
-        # /etc/iptables.d/#{table}/#{chain}/#{rule}.rule_v#{ip_version}
-        table, chain, filename = path.split('/')[3..5]
-        rule = ::File.basename(filename)
-
-        # IPv6 doesn't support nat
-        next if table == 'nat' and ip_version == 6
-
-        # Create hashes unless they already exist, and add the rule
-        rules[table] ||= {}
-        rules[table][chain] ||= {}
-        rules[table][chain][rule] = ::File.read(path)
-      end
-
-      iptables_restore = ''
-      rules.each do |table, chains|
-        iptables_restore << "*#{table}\n"
-
-        # Get default policies and rules for this chain
-        default_policies = chains.inject({}) {|new_chain, rule| new_chain[rule[0]] = rule[1].select{|k, v| k == 'default'}; new_chain }
-        all_chain_rules  = chains.inject({}) {|new_chain, rule| new_chain[rule[0]] = rule[1].reject{|k, v| k == 'default'}; new_chain }
-
-        # Apply default policies first
-        default_policies.each do |chain, policy|
-          iptables_restore << ":#{chain} #{policy['default'].chomp}\n"
-        end
-
-        # Apply rules for this chain, but sort before adding
-        all_chain_rules.each do |chain, chain_rules|
-          chain_rules.values.sort.each { |r| iptables_restore << "#{r.chomp}\n" }
-        end
-
-        iptables_restore << "COMMIT\n"
-      end
-
-      Chef::Resource::File.new(node['iptables-ng']["script_ipv#{ip_version}"], run_context).tap do |file|
-        file.owner('root')
-        file.group('root')
-        file.mode(00600)
-        file.content(iptables_restore)
-        file.run_action(:create)
-      end
-    end
-
-
     def restart_service(ip_version)
       # restart iptables service if available
       if node['iptables-ng']["service_ipv#{ip_version}"]
