a2ab52670c67840d868cb3ffa4ccc7f8bfa4d6c9
Chris Aumann <me@chr4.org>
2013-09-13 18:31:07 +0200

    bump version to 1.0.0 and add supported platforms

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,10 @@ maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures iptables-ng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
+
+%w{ubuntu debian
+   redhat centos amazon suse scientific
+   fedora gentoo arch}.each do |os|
+  supports os
+end
