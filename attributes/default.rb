f065d13d287d44adfc36a9397308ba637681a310
Chris Aumann <me@chr4.org>
2015-08-21 15:48:33 +0200

    Make nested if more readable

--- attributes/default.rb
+++ attributes/default.rb
@@ -61,19 +61,19 @@ when 'debian'
   # Debian squeeze (and before) only support an outdated version
   # of iptables-persistent, which is not capable of ipv6.
   # Furthermore, restarting the service doesn't properly reload the rules
+  if node['platform_version'].to_f >= 8.0
+    default['iptables-ng']['service_ipv4'] = 'netfilter-persistent'
+    default['iptables-ng']['service_ipv6'] = 'netfilter-persistent'
+  elsif node['platform_version'].to_f >= 7.0
+    default['iptables-ng']['service_ipv4'] = 'iptables-persistent'
+    default['iptables-ng']['service_ipv6'] = 'iptables-persistent'
+  end
+
   if node['platform_version'].to_f < 7.0
     # default['iptables-ng']['service_ipv4'] = 'iptables-persistent'
     default['iptables-ng']['script_ipv4'] = '/etc/iptables/rules'
     default['iptables-ng']['script_ipv6'] = '/etc/iptables/rules.v6'
   else
-    if node['platform_version'].to_f >= 8.0
-      default['iptables-ng']['service_ipv4'] = 'netfilter-persistent'
-      default['iptables-ng']['service_ipv6'] = 'netfilter-persistent'
-    else
-      default['iptables-ng']['service_ipv4'] = 'iptables-persistent'
-      default['iptables-ng']['service_ipv6'] = 'iptables-persistent'
-    end
-
     default['iptables-ng']['script_ipv4'] = '/etc/iptables/rules.v4'
     default['iptables-ng']['script_ipv6'] = '/etc/iptables/rules.v6'
   end
