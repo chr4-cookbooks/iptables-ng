9db04308cb2c736d199997eba3d9f3026be13edd
Nathan Williams <nathan@teamtreehouse.com>
2015-08-21 19:50:42 -0700

    rewrite default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,96 +18,86 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Which IP versions to manage rules for
-default['iptables-ng']['enabled_ip_versions'] = [4, 6]
+default['iptables-ng'].tap do |ip|
+  # Which IP versions to manage rules for
+  ip['enabled_ip_versions'] = [4, 6]
 
-# Which tables to manage:
-# When using containered setup (OpenVZ, Docker, LXC) it might might be
-# necessary to remove the "nat" and "raw" tables.
-default['iptables-ng']['enabled_tables'] = %w(nat filter mangle raw)
+  # Which tables to manage:
+  # When using containered setup (OpenVZ, Docker, LXC) it might might be
+  # necessary to remove the "nat" and "raw" tables.
+  ip['enabled_tables'] = %w( nat filter mangle raw )
 
-# Configure whether the service should be managed by Chef
-# /!\ Be careful when using this feature as it might leave your server in an inconsistent state.
-# See https://github.com/chr4-cookbooks/iptables-ng/pull/42 for more details.
-default['iptables-ng']['managed_service'] = true
+  # Configure whether the service should be managed by Chef
+  # /!\ Be careful when using this feature as it might leave your server in an inconsistent state.
+  # See https://github.com/chr4-cookbooks/iptables-ng/pull/42 for more details.
+  ip['managed_service'] = true
 
-# Enable nat support for ipv6
-# Older distributions do not support ipv6 nat, but recent Ubuntu does
-default['iptables-ng']['ip6tables_nat_support'] = value_for_platform(
-  'ubuntu' => { '14.04' => true, '14.10' => true, 'default' => false },
-  'default' => false,
-)
+  # Enable nat support for ipv6
+  # Older distributions do not support ipv6 nat, but recent Ubuntu does
+  ip['ip6tables_nat_support'] = value_for_platform(
+    'ubuntu' => { '14.04' => true, '14.10' => true, 'default' => false },
+    'default' => false,
+  )
 
-# Packages to install
-default['iptables-ng']['packages'] = case node['platform_family']
-when 'debian'
-  %w(iptables iptables-persistent)
-when 'rhel'
-  if node['platform'] == 'amazon'
-    # Amazon Linux doesn't include "iptables-services" or "iptables-ipv6"
-    %w(iptables)
-  elsif node['platform_version'].to_f >= 7.0
-    %w(iptables iptables-services)
-  else
-    %w(iptables iptables-ipv6)
-  end
-else
-  %w(iptables)
-end
+  # Packages to install
+  ip['packages'] =
+    case node['platform_family']
+    when 'debian'
+      %w( iptables iptables-persistent )
+    when 'rhel'
+      if node['platform'] == 'amazon'
+        %w( iptables )
+      elsif node['platform_version'].to_f >= 7.0
+        %w( iptables iptables-services )
+      else
+        %w( iptables iptables-ipv6 )
+      end
+    else
+      %w( iptables )
+    end
 
-# Where the rules are stored and how they are executed
-case node['platform']
-when 'debian'
-  # Debian squeeze (and before) only support an outdated version
-  # of iptables-persistent, which is not capable of ipv6.
-  # Furthermore, restarting the service doesn't properly reload the rules
-  if node['platform_version'].to_f >= 8.0
-    default['iptables-ng']['service_ipv4'] = 'netfilter-persistent'
-    default['iptables-ng']['service_ipv6'] = 'netfilter-persistent'
-  elsif node['platform_version'].to_f >= 7.0
-    default['iptables-ng']['service_ipv4'] = 'iptables-persistent'
-    default['iptables-ng']['service_ipv6'] = 'iptables-persistent'
-  end
+  # Services to manage, if any
+  ip['service_ipv4'], ip['service_ipv6'] =
+    case node['platform']
+    when 'debian'
+      # Debian squeeze (and before) only support an outdated version
+      # of iptables-persistent, which is not capable of ipv6.
+      # Furthermore, restarting the service doesn't properly reload the rules
+      if node['platform_version'].to_f >= 8.0
+        %w( netfilter-persistent netfilter-persistent )
+      elsif node['platform_version'].to_f >= 7.0
+        %w( iptables-persistent iptables-persistent )
+      else
+        %w()
+      end
+    when 'ubuntu'
+      if node['platform_version'].to_f >= 14.10
+        %w( netfilter-persistent netfilter-persistent )
+      else
+        %w( iptables-persistent iptables-persistent )
+      end
+    when 'redhat', 'centos', 'scientific', 'amazon', 'fedora', 'gentoo', 'arch'
+      %w( iptables ip6tables )
+    else
+      %w()
+    end
 
-  if node['platform_version'].to_f < 7.0
-    # default['iptables-ng']['service_ipv4'] = 'iptables-persistent'
-    default['iptables-ng']['script_ipv4'] = '/etc/iptables/rules'
-    default['iptables-ng']['script_ipv6'] = '/etc/iptables/rules.v6'
-  else
-    default['iptables-ng']['script_ipv4'] = '/etc/iptables/rules.v4'
-    default['iptables-ng']['script_ipv6'] = '/etc/iptables/rules.v6'
-  end
-
-when 'ubuntu'
-  if node['platform_version'].to_f >= 14.10
-    default['iptables-ng']['service_ipv4'] = 'netfilter-persistent'
-    default['iptables-ng']['service_ipv6'] = 'netfilter-persistent'
-  else
-    default['iptables-ng']['service_ipv4'] = 'iptables-persistent'
-    default['iptables-ng']['service_ipv6'] = 'iptables-persistent'
-  end
-  default['iptables-ng']['script_ipv4'] = '/etc/iptables/rules.v4'
-  default['iptables-ng']['script_ipv6'] = '/etc/iptables/rules.v6'
-
-when 'redhat', 'centos', 'scientific', 'amazon', 'fedora'
-  default['iptables-ng']['service_ipv4'] = 'iptables'
-  default['iptables-ng']['service_ipv6'] = 'ip6tables'
-  default['iptables-ng']['script_ipv4'] = '/etc/sysconfig/iptables'
-  default['iptables-ng']['script_ipv6'] = '/etc/sysconfig/ip6tables'
-
-when 'gentoo'
-  default['iptables-ng']['service_ipv4'] = 'iptables'
-  default['iptables-ng']['service_ipv6'] = 'ip6tables'
-  default['iptables-ng']['script_ipv4'] = '/var/lib/iptables/rules-save'
-  default['iptables-ng']['script_ipv6'] = '/var/lib/ip6tables/rules-save'
-
-when 'arch'
-  default['iptables-ng']['service_ipv4'] = 'iptables'
-  default['iptables-ng']['service_ipv6'] = 'ip6tables'
-  default['iptables-ng']['script_ipv4'] = '/etc/iptables/iptables.rules'
-  default['iptables-ng']['script_ipv6'] = '/etc/iptables/ip6tables.rules'
-
-else
-  default['iptables-ng']['script_ipv4'] = '/etc/iptables-rules.ipt'
-  default['iptables-ng']['script_ipv6'] = '/etc/ip6tables-rules.ipt'
+  # Where the rules are stored
+  ip['script_ipv4'], ip['script_ipv6'] =
+    case node['platform']
+    when 'debian'
+      if node['platform_version'].to_f < 7.0
+        %w( /etc/iptables/rules /etc/iptables/rules.v6 )
+      else
+        %w( /etc/iptables/rules.v4 /etc/iptables/rules.v6 )
+      end
+    when 'redhat', 'centos', 'scientific', 'amazon', 'fedora'
+      %w( /etc/sysconfig/iptables /etc/sysconfig/ip6tables )
+    when 'gentoo'
+      %w( /var/lib/iptables/rules-save /var/lib/ip6tables/rules-save )
+    when 'arch'
+      %w( /etc/iptables/iptables.rules /etc/iptables/ip6tables.rules )
+    else
+      %w( /etc/iptables-rules.ipt /etc/ip6tables-rules.ipt )
+    end
 end
