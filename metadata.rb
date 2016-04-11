194365eadad5dfd1a66e24395c0d8b8cc7d0ca5a
Arvid Warnecke <arvid.warnecke@otto.de>
2016-04-11 21:29:48 +0200

    Fixed rubocop and foodcritic issues to get a passing build.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures iptables-ng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/chr4-cookbooks/iptables-ng'
+issues_url       'https://github.com/chr4-cookbooks/iptables-ng/issues'
 version          '2.2.8'
 
 %w(ubuntu debian
