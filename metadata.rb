040f333cf829dda9f148cf91089e9f3d38cfe2e8
Chris Aumann <me@chr4.org>
2016-10-11 13:20:22 +0200

    Bump version to 3.0.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures iptables-ng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/chr4-cookbooks/iptables-ng' if respond_to?(:source_url)
 issues_url       'https://github.com/chr4-cookbooks/iptables-ng/issues' if respond_to?(:issues_url)
-version          '2.3.1'
+version          '3.0.0'
 
 %w(ubuntu debian
    redhat centos amazon suse scientific
