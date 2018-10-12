78bf87b4b2b99b71e802715ccb33a7feca367061
Cédric Félizard <cedric@felizard.fr>
2018-10-12 00:20:28 +0000

    Ensure standardized license defined in metadata (FC069)
    http://www.foodcritic.io/#FC069

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'iptables-ng'
 maintainer       'Chris Aumann'
 maintainer_email 'me@chr4.org'
-license          'GNU Public License 3.0'
+license          'GPL-3.0-or-later'
 description      'Installs/Configures iptables-ng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/chr4-cookbooks/iptables-ng'
