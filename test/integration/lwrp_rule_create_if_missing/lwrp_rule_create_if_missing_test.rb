2efc3db30f48e3ae8539cad29f3144252a255aba
Hans Rakers <hans@shoq.com>
2022-08-30 10:19:33 +0200

    Fix rule_create_if_missing Inspec tests

--- test/integration/lwrp_rule_create_if_missing/lwrp_rule_create_if_missing_test.rb
+++ test/integration/lwrp_rule_create_if_missing/lwrp_rule_create_if_missing_test.rb
@@ -2,14 +2,14 @@
 describe file('/etc/iptables.d/filter/INPUT/ssh.rule_v4') do
   it { should exist }
   its('content') { should match('--protocol tcp --dport 22 --match state --state NEW --jump ACCEPT') }
-  its('content') { should match('--protocol tcp --dport 80 --match state --state NEW --jump ACCEPT') }
+  its('content') { should_not match('--protocol tcp --dport 80 --match state --state NEW --jump ACCEPT') }
 end
 
 # Should set SSH ip6tables rule
 describe file('/etc/iptables.d/filter/INPUT/ssh.rule_v6') do
   it { should exist }
   its('content') { should match('--protocol tcp --dport 22 --match state --state NEW --jump ACCEPT') }
-  its('content') { should match('--protocol tcp --dport 80 --match state --state NEW --jump ACCEPT') }
+  its('content') { should_not match('--protocol tcp --dport 80 --match state --state NEW --jump ACCEPT') }
 end
 
 service_ipv4 = case os.family
@@ -38,12 +38,12 @@ end
 # Should apply the specified iptables rules
 describe command('iptables -L -n') do
   its('stdout') { should match('tcp dpt:22 state NEW') }
-  its('stdout') { should match('tcp dpt:80 state NEW') }
+  its('stdout') { should_not match('tcp dpt:80 state NEW') }
   its('exit_status') { should eq 0 }
 end
 
 describe command('ip6tables -L -n') do
   its('stdout') { should match('tcp dpt:22 state NEW') }
-  its('stdout') { should match('tcp dpt:80 state NEW') }
+  its('stdout') { should_not match('tcp dpt:80 state NEW') }
   its('exit_status') { should eq 0 }
 end
